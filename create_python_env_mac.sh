a675a41ac27e2c4cbd429599efa0223747262cca
Jordan Christian <jordan.i.christian@und.edu>
2026-04-27 16:14:49 -0500

    Delete create_python_env_mac.sh

--- create_python_env_mac.sh
+++
@@ -1,5 +0,0 @@
-python3.11 -m venv und_atsc_env
-source und_atsc_env/bin/activate
-pip install -r requirements.txt
-python3.11 -m ipykernel install --user --name und_atsc_env --display-name und_atsc_kernel
-echo "ATSC Python environment setup complete!"
